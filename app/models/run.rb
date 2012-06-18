1a20506108ccfbbe7ee4c7c0e3ca79e59c516b8a
Michael Dever <michael.dever2@mail.dcu.ie>
2012-06-18 22:25:02 +0100

    validations for benchmarking

--- app/models/run.rb
+++ app/models/run.rb
@@ -2,4 +2,12 @@ class Run < ActiveRecord::Base
   attr_accessible :distill_compile_time, :distill_size, :ghc_compile_time, :ghc_size, :program_id, :super_compile_time, :super_size, :user_id
   has_many :run_point
   belongs_to :user
+  validates :distill_compile_time, :presence => true
+  validates :distill_size, :presence => true
+  validates :ghc_compile_time, :presence => true
+  validates :ghc_size, :presence => true
+  validates :super_compile_time, :presence => true
+  validates :super_size, :presence => true
+  validates :program_id, :presence => true
+  validates :user_id, :presence => true
 end
--- app/models/run_point.rb
+++ app/models/run_point.rb
@@ -3,4 +3,11 @@ class RunPoint < ActiveRecord::Base
   has_one :run_type
   belongs_to :run
   belongs_to :user, :through => :run
+  
+  validates :mem_size, :presence => true
+  validates :program_id, :presence => true
+  validates :run_id, :presence => true
+  validates :run_time, :presence => true
+  validates :run_type_id, :presence => true
+  validates :user_id, :presence => true
 end
