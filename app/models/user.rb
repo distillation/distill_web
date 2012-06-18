89d051e44b4688213975a328fea838c276724072
Michael Dever <dever@Michaels-MacBook-Air.local>
2012-06-18 20:16:32 +0100

    restyled account creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   acts_as_authentic
   attr_accessible :username, :email, :password, :password_confirmation
   has_many :programs
+  #validations taken care of by authlogic
 end
