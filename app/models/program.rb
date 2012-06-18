63fd82bd79eb0eb1c670036086de5f48f4c090bb
Michael Dever <dever@Michaels-MacBook-Air.local>
2012-06-18 20:16:46 +0100

    program validations

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,4 +3,11 @@ class Program < ActiveRecord::Base
   belongs_to :user
   accepts_nested_attributes_for :user
   attr_accessible :user_id, :name, :file_name, :arguments_file_name, :size, :lines, :number_of_levels, :number_of_runs, :file, :arguments_file
+  validates :user_id, :presence => true
+  validates :name, :presence => true
+  validates :file_name, :presence => true
+  validates :arguments_file_name, :presence => true
+  validates :size, :presence => true
+  validates :number_of_levels, :presence => true
+  validates :number_of_runs, :presence => true
 end
